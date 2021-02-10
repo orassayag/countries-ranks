cf9c3230ed88a85523fac2e1c74f016f06c065a3
Or Assayag <orassayag@gmail.com>
2021-02-10 17:29:29 +0200

    initiate project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './Layout.module.css';
+import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Brightness6Rounded } from '@material-ui/icons';
+
+const Layout = ({ children, title = "Countries Ranks" }) => {
+    const [theme, setTheme] = useState('light');
+    const setThemeState = (theme) => {
+        setTheme(theme);
+        localStorage.setItem('theme', theme);
+        document.documentElement.setAttribute('data-theme', theme);
+    };
+    const switchTheme = () => {
+        if (theme === 'light') {
+            setThemeState('dark');
+        }
+        else {
+            setThemeState('light');
+        }
+    };
+
+    useEffect(() => {
+        setThemeState(localStorage.getItem('theme'));
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{title}</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <header className={styles.header}>
+                <Link href="/">
+                    <div className={styles.header}>
+                        <FontAwesomeIcon
+                            className={styles.logo}
+                            icon={faGlobeEurope}
+                        />
+                        <div className={styles.headerTitle}>
+                            Countries Ranks
+                        </div>
+                    </div>
+                </Link>
+                <button className={styles.theme_switcher} onClick={switchTheme}>
+                    <Brightness6Rounded />
+                </button>
+            </header>
+            <main className={styles.main}>
+                {children}
+            </main>
+            <footer className={styles.footer}>
+                Or Assayag © Thanks to Thu Nghiem from devchallenges.io
+            </footer>
+        </div >
+    );
+};
+
+export default Layout;
